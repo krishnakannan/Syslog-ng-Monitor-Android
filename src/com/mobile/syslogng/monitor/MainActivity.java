3f02d2fb1b6638536dfb05e269abfbccedbdedc2
Krish <k.krish@yahoo.com>
2015-05-30 02:22:43 +0530

    First working version

--- src/com/mobile/syslogng/monitor/MainActivity.java
+++ src/com/mobile/syslogng/monitor/MainActivity.java
@@ -1,9 +1,15 @@
 package com.mobile.syslogng.monitor;
 
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +17,58 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		//Hide activity name on ActionBar
+		getActionBar().setDisplayShowTitleEnabled(false);
+		
+		//Collect the Instance Details
+		final Button button = (Button) findViewById(R.id.add_instance);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				String instanceDetails;
+				Integer portNumber;
+				Intent tempConnect;
+				EditText instanceText;
+				EditText portText;
+				
+				instanceText = (EditText) findViewById(R.id.instance_input);
+				portText = (EditText) findViewById(R.id.port_input);
+				tempConnect = new Intent(getApplicationContext(), TempConnector.class);
+				
+				instanceDetails = instanceText.getText().toString();
+				
+				if(instanceDetails == null || instanceDetails == "")
+				{
+					Toast toast = Toast.makeText(getApplicationContext(), "Enter valid host details", Toast.LENGTH_LONG);
+					toast.show();
+				}
+				else
+				{
+					
+					try
+					{
+						portNumber = Integer.parseInt(portText.getText().toString());
+						tempConnect.putExtra("Instance", instanceDetails);
+						tempConnect.putExtra("Port", portNumber);
+						Log.i("Host Instance Details ","URL -  "+ instanceDetails +" Port - "+ portNumber);
+						startActivity(tempConnect);
+					}
+					catch(Exception e)
+					{
+						Toast toast = Toast.makeText(getApplicationContext(), "Port entered is not a Number", Toast.LENGTH_LONG);
+						toast.show();	
+					}
+					finally
+					{
+						instanceText.setText("");
+						portText.setText("");
+					}
+					
+					
+				}			
+			}
+		});
 	}
 }
