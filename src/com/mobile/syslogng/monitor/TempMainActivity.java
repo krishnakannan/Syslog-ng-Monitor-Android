abf917593bfbf52fa9727da47670e10adeafa4b8
Krish <k.krish@yahoo.com>
2015-06-09 16:46:29 +0530

    Moving main activity aside

--- src/com/mobile/syslogng/monitor/TempMainActivity.java
+++ src/com/mobile/syslogng/monitor/TempMainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class TempMainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
